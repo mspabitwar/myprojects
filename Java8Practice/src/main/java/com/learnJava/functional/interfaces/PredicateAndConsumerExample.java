a437f4f720a2ee767c6ae86f7042b2c0f2f62450
mahesh <mspabitwar@gmail.com>
2020-07-05 19:54:46 +0530

    bipredicate changes

--- Java8Practice/src/main/java/com/learnJava/functional/interfaces/PredicateAndConsumerExample.java
+++ Java8Practice/src/main/java/com/learnJava/functional/interfaces/PredicateAndConsumerExample.java
@@ -2,6 +2,7 @@ package com.learnJava.functional.interfaces;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -10,13 +11,19 @@ import com.learnJava.data.StudentDataBase;
 
 public class PredicateAndConsumerExample {
 
-	private static Predicate<Student> gradeFilter = (student) -> student.getGradeLevel() >= 3;
-	private static Predicate<Student> gpaFilter = (student) -> student.getGpa() >= 3.9;
+	Predicate<Student> gradeFilter = (student) -> student.getGradeLevel() >= 3;
+	Predicate<Student> gpaFilter = (student) -> student.getGpa() >= 3.9;
+	
+	BiPredicate<Integer, Double> studentBiPredicate = (gradeLevel, gpa) -> gradeLevel >= 3 && gpa >= 3.9;
 	
 	BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println(name + " : " + activities);
 	
 	Consumer<Student> studentConsumer = (student) -> {
 		if(gradeFilter.and(gpaFilter).test(student)) {
+			studentBiConsumer.accept(student.getName(), student.getActivities());
+		}
+		
+		if(studentBiPredicate.test(student.getGradeLevel(), student.getGpa())) {
 			studentBiConsumer.accept(student.getName(), student.getActivities());
 		}
 	};
