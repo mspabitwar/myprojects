b11515f96d74fb2b88bc716d6bba608ea74af0f5
mahesh <mspabitwar@gmail.com>
2020-11-22 19:01:12 +0530

    array rotation using Juggling Algo

---
+++ GreeksForGreeksPractice/src/main/java/com/datastructure/arrays/rotation/RotateArrayUsingJugglingAlgo.java
@@ -0,0 +1,50 @@
+package com.datastructure.arrays.rotation;
+
+public class RotateArrayUsingJugglingAlgo {
+
+	public static void main(String[] args) {
+
+		RotateArrayUsingJugglingAlgo rotate = new RotateArrayUsingJugglingAlgo();
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+		rotate.leftRotate(arr, 2, 7);
+		rotate.printArray(arr, 7);
+	}
+
+	/* Function to left rotate arr[] of size n by d */
+	void leftRotate(int arr[], int d, int n) {
+
+		/* To handle if d >= n */
+		d = d % n;
+		int i, j, k, temp;
+		int g_c_d = gcd(d, n);
+		for (i = 0; i < g_c_d; i++) {
+			/* move i-th values of blocks */
+			temp = arr[i];
+			j = i;
+			while (true) {
+				k = j + d;
+				if (k >= n)
+					k = k - n;
+				if (k == i)
+					break;
+				arr[j] = arr[k];
+				j = k;
+			}
+			arr[j] = temp;
+		}
+	}
+
+	/* Fuction to get gcd of a and b */
+	int gcd(int a, int b) {
+		if (b == 0)
+			return a;
+		else
+			return gcd(b, a % b);
+	}
+
+	/* utility function to print an array */
+	void printArray(int arr[], int n) {
+		for (int i = 0; i < n; i++)
+			System.out.print(arr[i] + " ");
+	}
+}
