dd8dc7ee3ae262953158dee84b309e2cc734e34d
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-09-28 09:46:17 +0530

    java based configuration for producer and consumer

--- KafkaProducer/src/main/java/com/spring/kafka/config/KafkaProducerConfig.java
+++ KafkaProducer/src/main/java/com/spring/kafka/config/KafkaProducerConfig.java
@@ -1,14 +1,41 @@
 package com.spring.kafka.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaProducerConfig {
 
-    @Bean
-    public NewTopic createTopic() {
+	@Bean
+	public NewTopic createTopic() {
 		return new NewTopic("DemoTopic1", 5, (short) 1);
+	}
+
+	@Bean
+	public Map<String, Object> producerConfig() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+				"org.springframework.kafka.support.serializer.JsonSerializer");
+		return props;
+	}
+
+	@Bean
+	public ProducerFactory<String, Object> producerFactory() {
+		return new DefaultKafkaProducerFactory<>(producerConfig());
+	}
+
+	@Bean
+	public KafkaTemplate<String, Object> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
 	}
 }
