a11eb3c586babf2f107d7d0a6f0efd4889b0281a
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-09-29 07:52:36 +0530

    retry logic implemented for failure scenarios

--- kafka-retry-demo/src/main/java/com/spring/kafka/consumer/MessageListener.java
+++ kafka-retry-demo/src/main/java/com/spring/kafka/consumer/MessageListener.java
@@ -2,7 +2,11 @@ package com.spring.kafka.consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 import com.spring.kafka.dto.Customer;
@@ -13,10 +17,20 @@ public class MessageListener {
 	Logger logger = LoggerFactory.getLogger(MessageListener.class);
 
 	@KafkaListener(topics = "RetryDemoTopic", groupId = "RetryDemoGroup")
-	public void eventConsumer(Customer customer) {
-		if(customer.getEmail().contains("test")) {
+	@RetryableTopic(attempts = "4") // 3 (n-1) retries
+	public void eventConsumer(Customer customer, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+			@Header(KafkaHeaders.OFFSET) long offset) {
+
+		logger.info("Received : {} , from {} , offset {}", customer, topic, offset);
+
+		if (customer.getEmail().contains("test")) {
 			throw new RuntimeException("Invalid EmailId");
 		}
-		logger.info("Consumer consumed the event : {} ", customer);
+	}
+
+	@DltHandler
+	public void dltHandler(Customer customer, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+			@Header(KafkaHeaders.OFFSET) long offset) {
+		logger.info("DltHandler Received : {} , from {} , offset {}", customer, topic, offset);
 	}
 }
