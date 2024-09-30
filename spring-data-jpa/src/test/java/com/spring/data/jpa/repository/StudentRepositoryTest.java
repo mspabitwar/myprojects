3886f172b54f34326e309a66a3b2d5bc8b57be81
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-09-30 15:22:30 +0530

    spring data jpa demo project

---
+++ spring-data-jpa/src/test/java/com/spring/data/jpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.spring.data.jpa.repository;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.spring.data.jpa.entity.Student;
+
+@SpringBootTest
+class StudentRepositoryTest {
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Test
+	public void saveStudent() {
+		Student student = Student.builder().emailId("shabbir@gmail.com").firstName("Shabbir").lastName("Dawoodi")
+				.guardianName("Nikhil").guardianEmail("nikhil@gmail.com").guardianMobile("9999999999").build();
+		studentRepository.save(student);
+	}
+	
+	@Test
+	public void printAllStudent() {
+		List<Student> studentList = studentRepository.findAll();
+		System.out.println("studentList = " + studentList);
+	}
+}
