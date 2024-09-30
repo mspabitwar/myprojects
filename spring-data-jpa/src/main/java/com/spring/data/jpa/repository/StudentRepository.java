bf4e35a264b4e82cdb7d60402edaa21c248a1e06
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-09-30 16:18:27 +0530

    JPQL implementation

--- spring-data-jpa/src/main/java/com/spring/data/jpa/repository/StudentRepository.java
+++ spring-data-jpa/src/main/java/com/spring/data/jpa/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.spring.data.jpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.spring.data.jpa.entity.Student;
@@ -11,8 +12,16 @@ import com.spring.data.jpa.entity.Student;
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
 	public List<Student> findByFirstName(String firstName);
-	
+
 	public List<Student> findByFirstNameContaining(String name);
-	
+
 	public List<Student> findByGuardianName(String guardianName);
+
+	// JPQL
+	@Query("select s from Student s where s.emailId = ?1")
+	public Student getStudentByEmailAddress(String emailId);
+
+	// JPQL
+	@Query("select s.firstName from Student s where s.emailId = ?1")
+	public String getStudentFirstNameByEmailAddress(String emailId);
 }
