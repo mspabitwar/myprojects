e1c023b63bdd9771a7eb8394eb3201b9bc56db1c
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-10-01 06:23:13 +0530

    paging and sorting

--- spring-data-jpa/src/main/java/com/spring/data/jpa/repository/CourseRepository.java
+++ spring-data-jpa/src/main/java/com/spring/data/jpa/repository/CourseRepository.java
@@ -1,5 +1,7 @@
 package com.spring.data.jpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.spring.data.jpa.entity.Course;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
-	
+
+	Page<Course> findByTitleContaining(String title, Pageable pageable);
 }
