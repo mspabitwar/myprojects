8c90d3cb5db781632a40e720f256d2f7a2e68ea0
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-10-28 19:27:44 +0530

    interface main method

---
+++ Java8Practice/src/main/java/com/java/durga/concept08/staticMethods/StaticMethodMainDemo.java
@@ -0,0 +1,8 @@
+package com.java.durga.concept08.staticMethods;
+
+interface StaticMethodMainDemo {
+
+	public static void main(String[] args) {
+		System.out.println("interface main method");
+	}
+}
