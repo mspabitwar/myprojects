b4f3bfee5907c9dcd533ee61174273e1415e06d5
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-10-28 16:40:14 +0530

    local variable inside lambda must be final

--- Java8Practice/src/main/java/com/java/durga/concept06/anonymous/inner/classes/ThisKeywordDemo.java
+++ Java8Practice/src/main/java/com/java/durga/concept06/anonymous/inner/classes/ThisKeywordDemo.java
@@ -7,6 +7,7 @@ interface interf1 {
 public class ThisKeywordDemo {
 
 	int x = 888;
+	int a = 10;
 	
 	public static void main(String[] args) {
 
@@ -17,6 +18,7 @@ public class ThisKeywordDemo {
 
 	@SuppressWarnings("unused")
 	public void m2() {
+		int b = 20;
 
 		interf1 I1 = new interf1() {
 			int x = 999;
@@ -30,7 +32,11 @@ public class ThisKeywordDemo {
 		
 		interf1 I2 = () -> {
 			int x = 999;
+			a = 100;
+			//b = 200; //Local variable b defined in an enclosing scope must be final or effectively final
 			System.out.println("Lambda Expression : " + this.x);
+			System.out.println("Lambda Expression : " + a);
+			System.out.println("Lambda Expression : " + b);
 		};
 		I2.m1();
 	}
