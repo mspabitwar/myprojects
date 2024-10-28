ed49553f1d9f48ef52bf597f96fa8a4d15afebb8
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-10-28 21:57:40 +0530

    few more predicate

--- Java8Practice/src/main/java/com/java/durga/concept09/predicates/PredicateDemo.java
+++ Java8Practice/src/main/java/com/java/durga/concept09/predicates/PredicateDemo.java
@@ -1,13 +1,23 @@
 package com.java.durga.concept09.predicates;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.function.Predicate;
 
 public class PredicateDemo {
 
 	public static void main(String[] args) {
 
-		Predicate<Integer> predicate = number -> number > 10;
-		System.out.println(predicate.test(20));
-		System.out.println(predicate.test(8));
+		Predicate<Integer> numberChecker = number -> number > 10;
+		System.out.println(numberChecker.test(20));
+		System.out.println(numberChecker.test(8));
+		
+		Predicate<String> stringLengthChecker = s -> s.length() > 5;
+		System.out.println(stringLengthChecker.test("Hello World"));
+		System.out.println(stringLengthChecker.test("Hello"));
+		
+		Predicate<Collection<String>> collectionEmptinessChecker = c -> c.isEmpty();
+		System.out.println(collectionEmptinessChecker.test(Arrays.asList()));
+		System.out.println(collectionEmptinessChecker.test(Arrays.asList("test")));
 	}
 }
