1e9fabb090b20a1b15255d6268f9617e13815e5f
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-10-30 12:43:33 +0530

    Sorted method demo

---
+++ Java8Practice/src/main/java/com/java/durga/concept13/stream/SortedDemo.java
@@ -0,0 +1,21 @@
+package com.java.durga.concept13.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortedDemo {
+
+	public static void main(String[] args) {
+
+		List<Integer> numbers = Arrays.asList(0, 10, 20, 5, 15, 25);
+		System.out.println("Numbers before sorting" + numbers);
+
+		List<Integer> numbersSortedInAscOrder = numbers.stream().sorted().collect(Collectors.toList());
+		System.out.println("Numbers with default sorting" + numbersSortedInAscOrder);
+
+		List<Integer> numbersSortedInDescOrder = numbers.stream().sorted((n1, n2) -> n2 - n1)
+				.collect(Collectors.toList());
+		System.out.println("Numbers with default sorting" + numbersSortedInDescOrder);
+	}
+}
