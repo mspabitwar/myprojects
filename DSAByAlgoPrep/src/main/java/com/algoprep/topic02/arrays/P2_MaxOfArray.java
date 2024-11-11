57f6da57231a34bec758a62c2258508dfe1735bc
Mahesh Pabitwar <54490588+mspabitwar@users.noreply.github.com>
2024-11-11 20:29:49 +0530

    max element from arrays

---
+++ DSAByAlgoPrep/src/main/java/com/algoprep/topic02/arrays/P2_MaxOfArray.java
@@ -0,0 +1,29 @@
+package com.algoprep.topic02.arrays;
+
+import java.util.Scanner;
+
+public class P2_MaxOfArray {
+
+	public static void main(String[] args) {
+
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Enter size of array");
+			int size = scanner.nextInt();
+			int[] numbers = new int[size];
+
+			System.out.println("Enter elements of array");
+			for (int index = 0; index < size; index++) {
+				numbers[index] = scanner.nextInt();
+			}
+
+			int max = numbers[0];
+			// int max = Integer.MIN_VALUE;
+
+			for (int index = 1; index < size; index++) {
+				max = numbers[index] > max ? numbers[index] : max;
+			}
+
+			System.out.println("Max = " + max);
+		}
+	}
+}
